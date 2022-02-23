05f64bbbeac1b023f4f057c7f6798a703aa366b5
Alexis Breust <alexis.breust@gmail.com>
2022-02-23 09:05:36 +0100

    Sub variants.

---
+++ assets/materials/decorum.glsl
@@ -0,0 +1,25 @@
+#version 450
+
+// ------------------
+// ----- VERTEX -----
+
+#if defined(VERTEX)
+
+#include "$/forward-renderer/default.vert"
+
+// --------------------
+// ----- FRAGMENT -----
+
+#elif defined(FRAGMENT)
+
+#include "$/forward-renderer/default-header.frag"
+
+layout(std140, set = MATERIAL_DESCRIPTOR_SET_INDEX, binding = 0) uniform MaterialShaderObject {
+    vec3 color;
+} material;
+
+void main() {
+    outColor = vec4(material.color, 1.0);
+}
+
+#endif
