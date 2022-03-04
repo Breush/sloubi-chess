d5f23eb113f7cc2457dc3ad33164e29e420937b9
Alexis Breust <alexis.breust@gmail.com>
2022-03-04 15:10:20 +0100

    Parsing and displaying symbols and NAGs - Closes #6

--- assets/materials/text.glsl
+++ assets/materials/text.glsl
@@ -20,9 +20,18 @@ layout(set = MATERIAL_DESCRIPTOR_SET_INDEX, binding = 0) uniform MaterialShaderO
 
 layout(set = MATERIAL_DESCRIPTOR_SET_INDEX, binding = 1) uniform sampler2D fontTexture;
 
+vec3 toLinear(vec3 sRGB)
+{
+    bvec3 cutoff = lessThan(sRGB, vec3(0.04045));
+    vec3 higher = pow((sRGB + vec3(0.055)) / vec3(1.055), vec3(2.4));
+    vec3 lower = sRGB / vec3(12.92);
+
+    return mix(higher, lower, cutoff);
+}
+
 void main() {
     float opacity = texture(fontTexture, uv).r;
-    outColor = vec4(material.color, opacity);
+    outColor = vec4(toLinear(material.color), opacity);
 }
 
 #endif
