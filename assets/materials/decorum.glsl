00d03ec22069d4bc213103e401cf05e5b7aa8cee
Alexis Breust <alexis@breust.fr>
2022-08-20 21:31:33 +0200

    Added variation collapsing - Closes #11

---
+++ assets/materials/decorum.glsl
@@ -0,0 +1,37 @@
+#version 450
+
+// ------------------
+// ----- VERTEX -----
+
+#if defined(VERTEX)
+
+#include "$/forward-renderer/default.vert"
+
+// --------------------
+// ----- FRAGMENT -----
+
+#elif defined(FRAGMENT)
+
+#include "$/forward-renderer/default-header.frag"
+
+layout(std140, set = MATERIAL_DESCRIPTOR_SET_INDEX, binding = 0) uniform MaterialShaderObject {
+    vec3 color;
+    float height;
+    bool collapsed;
+} material;
+
+vec3 toLinear(vec3 sRGB)
+{
+    bvec3 cutoff = lessThan(sRGB, vec3(0.04045));
+    vec3 higher = pow((sRGB + vec3(0.055)) / vec3(1.055), vec3(2.4));
+    vec3 lower = sRGB / vec3(12.92);
+
+    return mix(higher, lower, cutoff);
+}
+
+void main() {
+    float alpha = material.collapsed ? 2 * abs(fract(uv.y * material.height) - 0.5) : 1.0;
+    outColor = vec4(toLinear(material.color.rgb), alpha);
+}
+
+#endif
